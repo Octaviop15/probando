eab45be7f9d8b410dac0e94770da42367b49e90c
Octaviop15 <31938374+Octaviop15@users.noreply.github.com>
2017-09-16 21:38:06 -0300

    Update PrimerRepositorioEnGitHub.java

--- Primer repositorio en GitHub/src/primer/repositorio/en/github/PrimerRepositorioEnGitHub.java
+++ Primer repositorio en GitHub/src/primer/repositorio/en/github/PrimerRepositorioEnGitHub.java
@@ -16,6 +16,7 @@ public class PrimerRepositorioEnGitHub {
      */
     public static void main(String[] args) {
         System.out.println("hola GitHub");
+        System.out.println("1");
         // TODO code application logic here
     }
     
